81bd5a9138493a395c1e3fc4f0087986289afbb8
h-spear <80024307+h-spear@users.noreply.github.com>
2023-03-27 13:28:26 +0900

    init: 초기 저장소 생성

---
+++ .github/ISSUE_TEMPLATE/feature_request.md
@@ -0,0 +1,18 @@
+---
+name: Feature request
+about: Suggest an idea for this project
+title: ''
+labels: ''
+assignees: ''
+
+---
+
+## Description
+설명
+
+## To-do
+ - todo1
+ - todo2
+
+## ETC
+설명
---
+++ src/test/java/prettypop/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package prettypop.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
