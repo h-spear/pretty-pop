a429f75e47375b8bfbb39e6e938d7d97b648b275
h-spear <80024307+h-spear@users.noreply.github.com>
2023-03-31 03:00:25 +0900

    test: 데이터 초기화용 컴포넌트 추가
    - 테스트를 위한 초기화 컴포넌트
    
    Related to #16

--- src/main/java/prettypop/shop/InitDb.java
+++ src/main/java/prettypop/shop/InitDb.java
@@ -4,10 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import prettypop.shop.entity.Member;
+import prettypop.shop.entity.*;
+import prettypop.shop.repository.ItemRepository;
 import prettypop.shop.repository.MemberRepository;
+import prettypop.shop.repository.ReviewRepository;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Component
 @RequiredArgsConstructor
@@ -26,20 +31,56 @@ public class InitDb {
     static class InitService {
 
         private final MemberRepository memberRepository;
+        private final ItemRepository itemRepository;
+        private final ReviewRepository reviewRepository;
         private final PasswordEncoder passwordEncoder;
 
         public void dbInit() {
             int memberCount = 10;
+            int itemCount = 20;
+            int reviewCountPerItem = 5;
+
+            List<Member> members = new ArrayList<>();
+            Random random = new Random();
+
             for (int i = 1; i <= memberCount; ++i) {
-                memberRepository.save(
+                Member member = memberRepository.save(
                         Member.builder()
-                                .username("member"+i)
+                                .username("member" + i)
                                 .name("name" + i)
                                 .nickname("nick" + i)
                                 .point(i)
                                 .password(passwordEncoder.encode("password"))
                                 .build()
                 );
+                members.add(member);
+            }
+
+            for (int i = 1; i <= itemCount; ++i) {
+                Item savedItem = itemRepository.save(
+                        Item.builder()
+                                .name("itemName" + i)
+                                .description("description..." + i)
+                                .stockQuantity(9999)
+                                .originalPrice(1000)
+                                .purchasePrice(900)
+                                .earnedPoint(50)
+                                .category(Category.category1)
+                                .thumbnailImageUrl("https://dummyimage.com/300x300/d4d4d4/454545.jpg&text=Product" + i)
+                                .itemStatus(ItemStatus.NEW)
+                                .build()
+                );
+                int reviewCount = random.nextInt(reviewCountPerItem);
+
+                // review 등록
+                for (int r = 1; r <= reviewCount; ++r) {
+                    reviewRepository.save(
+                            new Review(savedItem,
+                                    random.nextInt(6),
+                                    "review..." + r,
+                                    members.get(random.nextInt(members.size())))
+                    );
+                }
             }
         }
     }
