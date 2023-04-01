59c2a21c4cf57dc91a24b9f592c51fc61bc3619b
h-spear <80024307+h-spear@users.noreply.github.com>
2023-04-01 21:45:26 +0900

    fix: findByIdWithFetchJoin where절 추가
    where절이 누락되어 있었음

--- src/main/java/prettypop/shop/repository/OrderRepository.java
+++ src/main/java/prettypop/shop/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package prettypop.shop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import prettypop.shop.entity.Order;
 
 import java.util.Optional;
@@ -9,8 +10,10 @@ import java.util.Optional;
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("select distinct o from Order o" +
+            " left join fetch o.delivery " +
             " left join fetch o.orderItems oi" +
-            " left join fetch oi.item " +
-            " left join fetch o.member")
-    Optional<Order> findByIdWithFetchJoin(Long orderId);
+            " left join fetch oi.item" +
+            " left join fetch o.member" +
+            " where o.id = :orderId")
+    Optional<Order> findByIdWithFetchJoin(@Param("orderId") Long orderId);
 }
