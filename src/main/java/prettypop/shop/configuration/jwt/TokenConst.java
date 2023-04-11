613f4f31b69c257a01153e989a21b58088917f8d
h-spear <80024307+h-spear@users.noreply.github.com>
2023-04-11 13:58:06 +0900

    refactor: 스프링 시큐리티 Secret Key를 application.yml에서 설정

--- src/main/java/prettypop/shop/configuration/jwt/TokenConst.java
+++ src/main/java/prettypop/shop/configuration/jwt/TokenConst.java
@@ -1,7 +1,11 @@
 package prettypop.shop.configuration.jwt;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import java.util.Base64;
 
+@Component
 public class TokenConst {
     public static final String ACCESS_TOKEN = "Authorization";
     public static final String REFRESH_TOKEN = "Refresh_Token";
@@ -10,10 +14,16 @@ public class TokenConst {
     public static final int ACCESS_TOKEN_VALIDITY = 30 * 60;
     public static final int REFRESH_TOKEN_VALIDITY = 14 * 24 * 60 * 60;
 
-    private static final String ACCESS_SECRET_KEY_NOT_ENCODED = "prettypopaccesssecret";
-    private static final String REFRESH_SECRET_KEY_NOT_ENCODED = "prettypoprefreshsecret";
+    public static String ACCESS_SECRET_KEY;
+    public static String REFRESH_SECRET_KEY;
 
+    @Value("${my-security.access-secret-key}")
+    public void setAccessSecretKeyNotEncoded(String accessSecretKey) {
+        ACCESS_SECRET_KEY = Base64.getEncoder().encodeToString(accessSecretKey.getBytes());
+    }
 
-    public static final String ACCESS_SECRET_KEY = Base64.getEncoder().encodeToString(ACCESS_SECRET_KEY_NOT_ENCODED.getBytes());
-    public static final String REFRESH_SECRET_KEY = Base64.getEncoder().encodeToString(REFRESH_SECRET_KEY_NOT_ENCODED.getBytes());
+    @Value("${my-security.refresh-secret-key}")
+    public void setRefreshSecretKeyNotEncoded(String refreshSecretKey) {
+        REFRESH_SECRET_KEY = Base64.getEncoder().encodeToString(refreshSecretKey.getBytes());
+    }
 }
